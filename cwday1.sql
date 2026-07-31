eafcbb57b5a6b8287ffdee303a5ad1f8365177e5
mirudhula <mirudhu2401@gmail.com>
2026-07-31 12:51:28 +0530

    day2 class works

--- cwday1.sql
+++ cwday1.sql
@@ -1,6 +1,9 @@
-create table students (
-    id int,
-    name varchar(50),
+create database retail_analytics;
 
-)
-
+create table customers
+(
+    id int primary key,
+    full_name varchar(50),
+    gender ENUM('M','F'),
+    city varchar(30),
+    is_active boolean );
