a31821d8cf01e6ddb49ea96fc0577d05a0d5598c
mirudhula <mirudhu2401@gmail.com>
2026-07-31 14:30:28 +0530

    fix: correct customer table definition and enhance product and order tables

--- cwday1.sql
+++ cwday1.sql
@@ -2,8 +2,30 @@ create database retail_analytics;
 
 create table customers
 (
-    id int primary key,
-    full_name varchar(50),
-    gender ENUM('M','F'),
-    city varchar(30),
-    is_active boolean );
+    customer_id INT PRIMARY KEY,
+    full_name VARCHAR(50),
+    gender ENUM ('M','F'),
+    city VARCHAR(30),
+    is_active BOOLEAN );
+
+    CREATE TABLE products(
+    id INT PRIMARY KEY,
+    prodcut_name VARCHAR(100),
+    category VARCHAR(50),
+    price DECIMAL(10,2),
+    manufacture_year YEAR 
+    );
+   
+    ALTER TABLE products
+    ADD stock_count INT;
+
+CREATE TABLE orders(
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    total_amount DECIMAL(10,2));
+
+ALTER TABLE orders
+ADD CONSTRAINT fk_customer
+FOREIGN KEY (customer_id)
+REFERENCES customers(customer_id);
